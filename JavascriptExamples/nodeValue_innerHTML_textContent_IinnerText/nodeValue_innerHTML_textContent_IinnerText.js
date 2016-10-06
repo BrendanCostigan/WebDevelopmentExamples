30c13af06fc61fe03952cb28f244c653428b9889
digby <digby@LENOVO-T440P>
2016-10-06 18:53:19 +0300

    Renames folders

---
+++ JavascriptExamples/nodeValue_innerHTML_textContent_IinnerText/nodeValue_innerHTML_textContent_IinnerText.js
@@ -0,0 +1,11 @@
+
+$(document).ready(function () {
+
+  var element = document.getElementById('testValue');
+
+  console.log("innerHtml: ".concat(element.innerHTML));
+  console.log("textContent: ".concat(element.textContent));
+  console.log("innerText: ".concat(element.innerText));
+  // console.log("nodeValue: ".concat(element.nodeValue));
+})
+
